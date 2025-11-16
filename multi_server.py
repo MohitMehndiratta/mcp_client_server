8d3a601ac0dfd3edacfd5b747cfbfaf631f95c03
Mohit Mehndiratta <mohitmehndiratta.mm@gmail.com>
2025-11-16 18:27:46 +0530

    mcp-server_code

---
+++ multi_server.py
@@ -0,0 +1,17 @@
+import os
+import json
+import asyncio
+import streamlit as st
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, SystemMessage
+
+SERVERS={
+'expense':{
+    'transport':''
+}
+}
+
+
